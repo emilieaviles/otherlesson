abd90e8982d4dd364c6bae6e35ad4f72e7a23361
emilieaviles <43994902+emilieaviles@users.noreply.github.com>
2019-01-30 12:46:42 -0500

    1

---
+++ Lesson2/problem1/problem1.py
@@ -0,0 +1,5 @@
+classes = ["english","math","history","science","gym","health","advisory","production"]
+
+for thing in classes:
+	print("You have been enrolled in " + thing + ".")
+	print()
---
+++ Lesson2/problem2/problem2.py
@@ -0,0 +1,7 @@
+import time
+
+numbers = [3,5,9,17,123,666,1000,4321]
+
+for number in numbers:
+	print(2 * number)
+	time.sleep(.5)
